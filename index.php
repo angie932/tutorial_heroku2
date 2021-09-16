553559d4a994cf8ad611bd8a684b655fe98abce2
angie932 <tapiieroangii@gmail.com>
2021-09-15 19:16:22 -0500

    se añade el proyecto

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/estilos6.css">
+    <title>Document</title>
+</head>
+<body>
+    <section class="portafolio">
+        <div class="contenido">
+
+            <h2 class="titulo">Seccion de imagenes</h2>
+            <p class="p1">Los animales son una maravilla</p>
+
+            <div class="galeria-port">
+                <div class="imagen-port">
+                    <img src="images/imagen1.jpg">
+                    <div class="hover-galeria">
+                        <p>Tigre Albino</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="images/imagen2.png">
+                    <div class="hover-galeria">
+                        <p>Caballo</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="images/imagen3.png">
+                    <div class="hover-galeria">
+                        <p>Gato</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="images/imagen4.png">
+                    <div class="hover-galeria">
+                        <p>Frugivoro</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="images/imagen5.png">
+                    <div class="hover-galeria">
+                        <p>Koaala</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="images/imagen6.png">
+                    <div class="hover-galeria">
+                        <p>Conejo</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="images/imagen7.png">
+                    <div class="hover-galeria">
+                        <p>Camaleon</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="images/imagen8.png">
+                    <div class="hover-galeria">
+                        <p>Buho</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="images/imagen9.png">
+                    <div class="hover-galeria">
+                        <p>Oso Polar</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="images/imagen10.png">
+                    <div class="hover-galeria">
+                        <p>Hipopotamo</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="images/imagen11.png">
+                    <div class="hover-galeria">
+                        <p>Vaca</p>
+                    </div>
+                </div>
+
+                <div class="imagen-port">
+                    <img src="images/imagen12.png">
+                    <div class="hover-galeria">
+                        <p>Oso Panda</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
